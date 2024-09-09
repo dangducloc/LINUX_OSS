9b14200d90c0f80ba78716b12dcbe3d48a3bfba4
dangducloc <dangloc2110@gmail.com>
2024-09-10 02:03:45 +0700

    The First

---
+++ app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.json({
+        "demo":{
+            "success":true,
+            "msg":"Wellcome to My project!!!"
+        }
+    });
+});
+
+app.listen(port,()=>{
+    console.log(`running on: http://localhost:${port}`);
+});
