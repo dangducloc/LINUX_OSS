4d94ec8bc07f5f2a2d24e7bbb73c91d3286ae89d
dangducloc <dangloc2110@gmail.com>
2024-09-14 00:20:46 +0700

    Feature-2.1-login and signup

--- DB/connect.js
+++ DB/connect.js
@@ -19,6 +19,8 @@ function makePool() {
 }
 
 // Main functions for api
+
+// Get all cakes
 async function get_all_cakes(pool) {
 	const qr = `
 	SELECT IDFood,Food,Price,TypeID,Amount,img_src,info_Detail,Type 
@@ -28,7 +30,37 @@ async function get_all_cakes(pool) {
 	return rs;
 }
 
+// Get cake by id 
+async function get_cake ( pool , id ){
+	const qr = `
+	SELECT IDFood,Food,Price,TypeID,Amount,img_src,info_Detail,Type 
+	FROM food JOIN type_of_food 
+	ON TypeID = type_of_food.IDType
+	WHERE IDFood = ${id};`
+	const [rs] = await pool.query(qr);
+	return rs;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+//Sign up
+async function signUp(pool, name, pass, mail, tel) {
+    const sql = "INSERT INTO user_table(User_name,Mail,Phone,Pass) VALUES (?,?,?,?)";
+    const rs = await pool.query(sql, [name, mail, tel, pass]);
+	return rs 
+    // console.log("inserted")
+}
+
+//Login 
+async function Login(pool, name, pass) {
+    const [rs] = await pool.query("SELECT * FROM user_table WHERE User_name = ? AND Pass = ?", [name, pass]);
+    return rs[0];
+}
+
 module.exports = {
 	makePool,
-	get_all_cakes
+	get_all_cakes,
+	get_cake,
+	//login and signup
+	Login,
+	signUp
 };
--- router/index.js
+++ router/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const router = express.Router();
 const endpoits = require('../controller/index')
 
-//define api rourrter
+//define api rourte
+
+//get all cake and get cake by id
 router.get('/getCakes', endpoits.getCakes);
+router.get('/cake/:id',endpoits.getCake);
+
+// Sign-up route
+router.post('/signup', endpoits.signUp);
+
+// Login route
+router.post('/login', endpoits.login);
+router.post('/signup',endpoits.signUp);
 
 
 module.exports = router;
