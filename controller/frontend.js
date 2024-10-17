f1019abbf1cfcbf3db329f775fd49e60b15e7ad6
dangducloc <dangloc2110@gmail.com>
2024-10-17 19:38:23 +0700

    finish-food-detail-template

--- controller/frontend.js
+++ controller/frontend.js
@@ -1,7 +1,6 @@
 const checkCookie = require("./checkCookie");
 const path = require('path');
 const fs = require('fs');
-const { name } = require("ejs");
 const { log } = require("console");
 
 exports.imgs = async (req, res) => {
@@ -14,7 +13,7 @@ exports.imgs = async (req, res) => {
         res.status(404).send('File not found');
     }
 
-}
+};
 
 exports.index = async (req, res) => {
     const api = "http://192.168.30.138:3000/api/getCakes";
@@ -22,15 +21,47 @@ exports.index = async (req, res) => {
     const foods = await response.json();
     const check = checkCookie(req, req);
     if (check.success == true) {
-        return res.render("index", { foods: foods });
+        const User_name = check.user.User_name;
+        return res.render("index", { foods, User_name });
     } else {
         return res.redirect("/login");
     }
 };
+exports.detail = async (req, res) => {
+    const id = req.params.id;
+    const check = checkCookie(req, req);
+    if (check.success == true) {
 
+        const api1 = `http://192.168.30.138:3000/api/cake/${id}`;
+        const raw_food = await fetch(api1);
+        const food = await raw_food.json();
+
+        const raw_comments = await fetch(`http://192.168.30.138:3000/api/comments/${id}`, {
+            method: 'GET', // You can use 'GET' explicitly or leave it out since it's the default
+            headers: {
+                'Content-Type': 'application/json',
+                'Cookie': `user=${req.cookies['user']}` // Replace 'your_cookie_value' with the actual cookie value
+            },
+            credentials: 'include' // This ensures cookies are sent with the request
+        });
+        
+        const comments = await raw_comments.json();
+        const User_name = check.user.User_name
+        return res.render("detail", { food: food[0], User_name, comments });
+
+
+
+    } else {
+        return res.redirect("/login");
+    }
+};
 exports.login = async (req, res) => {
     return res.render("login");
-}
+};
+exports.logout = async (req, res) => {
+    res.clearCookie('user');
+    return res.redirect("/login");
+};
 exports.signup = async (req, res) => {
     return res.render("signup");
-}
+};
--- router/frontend.js
+++ router/frontend.js
@@ -5,8 +5,10 @@ const router = express.Router();
 
 router.use("/assets",express.static(path.join(__dirname, '../assets')));
 router.get("/home",frontend.index);
+router.get("/detail/:id",frontend.detail);
 router.get("/assets/imgs",frontend.imgs);
 router.get("/login",frontend.login);
+router.get("/logout",frontend.logout);
 router.get("/signup",frontend.signup);
 router.get("/demo",(req,res)=>{
     res.sendFile(path.join(__dirname, '../views/components.html'));
